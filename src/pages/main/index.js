bb82f6c094e28174fc299c981e78e1896a67700d
Valdir Silva <valdir.ti@gmail.com>
2019-09-05 09:28:02 -0300

    Armazenando no estado e listando os produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component{
+    
+    state = {
+        products: []
+    }
 
     //Ação executada logo que o componente é exibido em tela
     componentDidMount(){
@@ -11,9 +15,15 @@ export default class Main extends Component{
     loadProducts = async () => {
         const response = await api.get('/products');
         console.log(response.data.docs);
+        this.setState({ products: response.data.docs });
     }
 
     render(){
-        return <h2>Teste de página Main</h2>;
+        // return <h2>Total de produtos: {this.state.products.length}</h2>;
+        return <div className="product-list">
+            {this.state.products.map(product => (
+                <h2 key={product._id}>{product.title}</h2>
+            ))}
+        </div>
     }
 }
